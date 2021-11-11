59bdf6f4a8f8ef5333a4ce6931dab204fa77e3c7
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-11-11 01:07:30 -0800

    bubble sort

---
+++ Sorting/src/sorting/selectionsort.java
@@ -0,0 +1,19 @@
+package sorting;
+
+public class selectionsort {
+
+	public static void main(String[] args) {
+		
+		int[] arr= {3,1,4,5,2};
+		
+		sort(arr);
+		
+		System.out.println();
+		
+	}
+
+	static void sort(int[] arr) {
+		
+		
+	}
+}
